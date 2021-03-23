a5261b166bfa74f3ec77a4d703746a265cd171a8
sanjeev <sanjeevkumar9160@gmail.com>
2021-03-23 14:43:50 +0530

    directory structure using puppet

--- environments/test/manifests/site.pp
+++ environments/test/manifests/site.pp
@@ -1,5 +1,5 @@
 
-  file { ['/var/www']:
+  file { '/var/www':
     ensure => 'directory',
     owner   => root,
     group   => root,
@@ -13,14 +13,14 @@
     group   => root,
     mode    => '755'
   }
-    file { ['/var/www/test-app/releases']:
+    file {'/var/www/test-app/releases':
     ensure => 'directory',
     owner   => root,
     group   => root,
     mode    => '755'
   }
   
-  file { ['/var/www/test-app/shared']:
+  file {'/var/www/test-app/shared':
     ensure => 'directory',
     owner   => root,
     group   => root,
